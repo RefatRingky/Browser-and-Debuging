46c8ebf371e126cb858e717aa37494f908a7b1be
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-03-03 17:22:02 +0600

    final commit

---
+++ fecth.js
@@ -0,0 +1,33 @@
+minutes = document.getElementById('minutes')
+seconds = document.getElementById('seconds')
+milisec = document.getElementById('milisec')
+startBtn = document.getElementById('startBtn')
+stopBtn = document.getElementById('stopBtn')
+
+stopWatch = () => {
+  startBtn.style.display = 'none'
+  stopBtn.style.display = 'block'
+  minutes.innerText = 0
+  seconds.innerText = 0
+  milisec.innerText = 0
+
+  count = 0
+  watch = setInterval(() => {
+    milisec.innerText = count++
+    if (count > 99) {
+      count = 0
+      seconds.innerText++
+    }
+    if (seconds.innerText > 59) {
+      seconds.innerText = 0
+      minutes.innerText++
+    }
+  }
+    , 10
+  )
+}
+stop = () => {
+  clearInterval(watch)
+  startBtn.style.display = 'block'
+  stopBtn.style.display = 'none'
+}
